96c70058d9f9ca47f052298a1ebfd265a5d45253
tzzkenz <127670903+tzzkenz@users.noreply.github.com>
2024-08-03 16:54:24 +0530

    Fixed the path of logo

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 function Home() {
   const [username, setUsername] = useState("");
@@ -15,23 +16,25 @@ function Home() {
   };
 
   return (
-    <div>
+    <>
       <header>
-        <img src="D:\hackn8\github-api\Charlies-angels\client\src\assets\logo.png" alt="Logo" className="logo" />
+        <img src={logo} alt="Logo" className="logo" />
       </header>
-      <form onSubmit={handleSubmit}>
-        <div className="center-container">
-          <div className="form-group">
-            <input
-              placeholder="Enter Github Username"
-              onChange={handleChange}
-              value={username}
-            />
-            <button type="submit">Submit</button>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className="center-container">
+            <div className="form-group">
+              <input
+                placeholder="Enter Github Username"
+                onChange={handleChange}
+                value={username}
+              />
+              <button type="submit">Submit</button>
+            </div>
           </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   );
 }
 
